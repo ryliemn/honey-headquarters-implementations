6ef012174f92f37bfb922dfc540d32590884e905
rylie <ryliemn@gmail.com>
2016-10-10 19:57:17 -0700

    basic menu appearance, filling in selects from data retreived from endpoint

--- jQuery/honeyHeadquarters.js
+++ jQuery/honeyHeadquarters.js
@@ -1,4 +1,27 @@
 // $.ajax('https://sheetsu.com/apis/v1.0/64b5c3f8').done(function(data) {
-$.ajax('http://localhost:3000/citizens').done(function(data) {
-  console.log(data);
+$.ajax('http://localhost:3000/citizens').done(function(citizens) {
+  function getUniqueValuesForField(field, citizens) {
+    const values = ['All'];
+    citizens.forEach((c) => {
+      let v = c[field];
+      if (!values.includes(v)) {
+        values.push(v);
+      }
+    });
+    return values;
+  }
+
+  function populateSelectWithOptions(select, options) {
+    options.forEach((option) => {
+      $(select).append($("<option>", { value: option, html: option }));
+    });
+  }
+
+  const characters = getUniqueValuesForField('character', citizens);
+  const hometowns = getUniqueValuesForField('hometown', citizens);
+  const sizes = getUniqueValuesForField('size', citizens);
+
+  populateSelectWithOptions("#character-select", characters);
+  populateSelectWithOptions("#hometown-select", hometowns);
+  populateSelectWithOptions("#size-select", sizes);
 });
